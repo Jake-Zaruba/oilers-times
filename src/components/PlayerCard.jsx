db6c39aa0e335a40c45f7f22e66de83970ff786d
Jake-Zaruba <94652629+Jake-Zaruba@users.noreply.github.com>
2023-10-28 14:45:27 -0500

    Added an animated home page. Added stats to the team card page.

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,8 +1,7 @@
-import PlayerStats from "./PlayerStats";
 import "./playerCard.css";
 import { Link } from "react-router-dom";
 
-export default function PlayerCard({ player }) {
+export default function PlayerCard({ player, winrate, kda, children }) {
   let url = "";
   switch (player.username) {
     case "Luden": {
@@ -17,12 +16,24 @@ export default function PlayerCard({ player }) {
       url = "TrillCosby-11719";
       break;
     }
+    case "BankRobber": {
+      url = "BankRobber-11133";
+      break;
+    }
+    case "SLEVZ": {
+      url = "SLEVZ-11689";
+      break;
+    }
   }
 
+  // console.log(stats);
+
+  // const { winrate, kda } = stats;
+
   return (
-    <>
+    <div style={{ position: "relative" }}>
+      {children}
       <div className="card-container">
-        {/* <img className="oiler-image" src={imageUrl}></img> */}
         <h2>{player.username}</h2>
         <div
           style={{
@@ -34,11 +45,24 @@ export default function PlayerCard({ player }) {
           <img style={{ borderRadius: "50%" }} src={player.avatar} />
           <p style={{ fontSize: "1.2rem" }}>{player.title}</p>
         </div>
-        <span></span>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "0px",
+            fontWeight: "700",
+            lineHeight: "1",
+          }}
+        >
+          <span>Winrate: {winrate}%</span>
+          {/* <span>K/D: {kda}</span> */}
+        </div>
+
         <Link to={url}>
           <button>Stats</button>
         </Link>
       </div>
-    </>
+    </div>
   );
 }
--- src/components/TeamCards.jsx
+++ src/components/TeamCards.jsx
@@ -4,7 +4,7 @@ import "./teamCards.css";
 
 export async function playerLoader() {
   const players = await Promise.all([
-    //FETCH JAKE
+    //FETCH JAKE PROFILE
     fetch(`https://overfast-api.tekrop.fr/players/Luden-11761/summary`).then(
       (res) => res.json().then((data) => data)
     ),
@@ -16,6 +16,38 @@ export async function playerLoader() {
     fetch(
       `https://overfast-api.tekrop.fr/players/TrillCosby-11719/summary`
     ).then((res) => res.json().then((data) => data)),
+    //FETCH SETH ALT
+    fetch(
+      `https://overfast-api.tekrop.fr/players/BankRobber-11133/summary`
+    ).then((res) => res.json().then((data) => data)),
+    //FETCH ERIK ALT
+    fetch(`https://overfast-api.tekrop.fr/players/SLEVZ-11689/summary`).then(
+      (res) => res.json().then((data) => data)
+    ),
+    //FETCH JAKE
+    fetch(`https://overfast-api.tekrop.fr/players/Luden-11761/summary`).then(
+      (res) => res.json().then((data) => data)
+    ),
+    //FETCH JAKE
+    fetch(`https://overfast-api.tekrop.fr/players/Luden-11761/summary`).then(
+      (res) => res.json().then((data) => data)
+    ),
+    //FETCH JAKE
+    fetch(`https://overfast-api.tekrop.fr/players/Luden-11761/summary`).then(
+      (res) => res.json().then((data) => data)
+    ),
+    //FETCH JAKE
+    fetch(`https://overfast-api.tekrop.fr/players/Luden-11761/summary`).then(
+      (res) => res.json().then((data) => data)
+    ),
+    //FETCH JAKE
+    fetch(`https://overfast-api.tekrop.fr/players/Luden-11761/summary`).then(
+      (res) => res.json().then((data) => data)
+    ),
+    //FETCH DYLAN
+    // fetch(
+    //   `https://overfast-api.tekrop.fr/players/dylaodylao-1369/summary`
+    // ).then((res) => res.json().then((data) => data)),
     // //FETCH RYAN
     // fetch(`https://overfast-api.tekrop.fr/players/DALEDANTONY-1212/summary`).then(
     //   (res) => res.json().then((data) => data)
@@ -28,26 +60,160 @@ export async function playerLoader() {
     // fetch(`https://overfast-api.tekrop.fr/players/itwilda-1717/summary`).then(
     //   (res) => res.json().then((data) => data)
     // ),
+    //FETCH TANNER
+    // fetch(`https://overfast-api.tekrop.fr/players/TnTaves-1528/summary`).then(
+    //   (res) => res.json().then((data) => data)
+    // ),
+    //FETCH TANNER
+    // fetch(`https://overfast-api.tekrop.fr/players/TnTaves-1528/summary`).then(
+    //   (res) => res.json().then((data) => data)
+    // ),
+  ]);
+
+  const stats = await Promise.all([
+    //FETCH JAKE STATS
+    fetch(
+      `https://overfast-api.tekrop.fr/players/Luden-11761/stats/summary`
+    ).then((res) => res.json().then((data) => data)),
+    //FETCH SETH STATS
+    fetch(
+      `https://overfast-api.tekrop.fr/players/RickeyRansom-1434/stats/summary`
+    ).then((res) => res.json().then((data) => data)),
+    //FETCH ERIK STATS
+    fetch(
+      `https://overfast-api.tekrop.fr/players/TrillCosby-11719/stats/summary`
+    ).then((res) => res.json().then((data) => data)),
+    //FETCH ERIK STATS
+    fetch(
+      `https://overfast-api.tekrop.fr/players/TrillCosby-11719/stats/summary`
+    ).then((res) => res.json().then((data) => data)),
+    //FETCH ERIK STATS
+    fetch(
+      `https://overfast-api.tekrop.fr/players/TrillCosby-11719/stats/summary`
+    ).then((res) => res.json().then((data) => data)),
+    //FETCH JAKE STATS
+    fetch(
+      `https://overfast-api.tekrop.fr/players/Luden-11761/stats/summary`
+    ).then((res) => res.json().then((data) => data)),
+    //FETCH JAKE STATS
+    fetch(
+      `https://overfast-api.tekrop.fr/players/Luden-11761/stats/summary`
+    ).then((res) => res.json().then((data) => data)),
+    //FETCH JAKE STATS
+    fetch(
+      `https://overfast-api.tekrop.fr/players/Luden-11761/stats/summary`
+    ).then((res) => res.json().then((data) => data)),
+    //FETCH JAKE STATS
+    fetch(
+      `https://overfast-api.tekrop.fr/players/Luden-11761/stats/summary`
+    ).then((res) => res.json().then((data) => data)),
+    //FETCH JAKE STATS
+    fetch(
+      `https://overfast-api.tekrop.fr/players/Luden-11761/stats/summary`
+    ).then((res) => res.json().then((data) => data)),
   ]);
 
   const resolvedPlayers = await Promise.resolve(players);
+  const resolvedStats = await Promise.resolve(stats);
 
-  return resolvedPlayers;
+  return [resolvedPlayers, resolvedStats];
 }
 
 export default function TeamCards() {
-  const players = useLoaderData();
-  const urls = ["Luden-11761", "RickeyRansom-1434", "TrillCosby-11719"];
-  const [jake, seth, erik] = players;
-  console.log(jake, seth, erik);
+  const [players, stats] = useLoaderData();
 
-  const oilers = players.map((player) => {
-    return <PlayerCard player={player} />;
+  const teamOneStats = stats.slice(0, 5);
+  const { general } = teamOneStats;
+  const teamTwoStats = stats.slice(5, 10);
+
+  const teamOne = players.slice(0, 5);
+  const teamTwo = players.slice(5, 10);
+
+  const teamOneMappedStats = teamOneStats.map((item) => {
+    return item.general;
+  });
+
+  const teamTwoMappedStats = teamTwoStats.map((item) => {
+    return item.general;
+  });
+
+  const houstonOilersStats = teamOneStats.map((item) => {
+    return item.general.winrate;
+  });
+
+  const memphisRobbersStats = teamTwoStats.map((item) => {
+    return item.general.winrate;
+  });
+
+  const houstonOilersWinrate = parseFloat(
+    0.2 *
+      houstonOilersStats.reduce(
+        (accumulator, currentValue) => accumulator + currentValue
+      )
+  ).toFixed(2);
+
+  const memphisRobbersWinrate = parseFloat(
+    0.2 *
+      memphisRobbersStats.reduce(
+        (accumulator, currentValue) => accumulator + currentValue
+      )
+  ).toFixed(2);
+
+  const houstonOilers = teamOne.map((player, index) => {
+    return (
+      <PlayerCard player={player} winrate={teamOneMappedStats[index].winrate}>
+        <div
+          className={
+            teamOneMappedStats[index].winrate >
+            teamTwoMappedStats[index].winrate
+              ? "team-card-line team-card-line-green"
+              : "team-card-line team-card-line-red"
+          }
+        ></div>
+      </PlayerCard>
+    );
+  });
+
+  const memphisRobbers = teamTwo.map((player, index) => {
+    return (
+      <>
+        <PlayerCard
+          player={player}
+          winrate={teamTwoMappedStats[index].winrate}
+        />
+      </>
+    );
   });
 
   return (
     <div className="team-cards-page-container">
-      <div className="team-cards-container">{oilers}</div>
+      <div className="team-cards-teams-container">
+        <div className="team-cards-container">{houstonOilers}</div>
+        {/* <div className="teams-text-container">
+          <h1 className="teams-text teams-text-header">HOUSTON OILERS</h1>
+          <h3 className="teams-text teams-text-subtext">VS</h3>
+          <h1 className="teams-text teams-text-header">MEMPHIS ROBBERS</h1>
+        </div> */}
+        <div className="team-cards-container">{memphisRobbers}</div>
+      </div>
+      <div
+        className="stat-favor-indicator"
+        style={
+          houstonOilersWinrate > memphisRobbersWinrate
+            ? {
+                top: "4rem",
+                background:
+                  "linear-gradient(to bottom, #95CC59, #95CC59 10%,transparent 90%,transparent)",
+                opacity: "70%",
+              }
+            : {
+                bottom: "0rem",
+                background:
+                  "linear-gradient(to top, #B71138, #B71138 10%,transparent 90%,transparent)",
+                opacity: "70%",
+              }
+        }
+      ></div>
     </div>
   );
 }
